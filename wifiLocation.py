b64aa9bdcc9f725893308ad66b02b7c61fac74b8
haokx1993 <35585044+haokx1993@users.noreply.github.com>
2018-01-25 14:59:12 +0800

    add comment

--- wifiLocation.py
+++ wifiLocation.py
@@ -18,7 +18,7 @@ import time
 import str_bytes_conv
 import formatapinfo
 
-# ESP8266 COM25 baudrate: 115200 just in haokx's PC
+# ESP8266 COM25 or COM26 baudrate: 115200 just in haokx's PC
 comNo = "COM26"
 baudrateCom = 115200
 
@@ -26,32 +26,43 @@ baudrateCom = 115200
 # amap key
 # my iPhone IMEI
 
-# Set up and open serial 
-wifiSerial = serial.Serial(comNo, baudrateCom, timeout = 3)
-print(wifiSerial.portstr)
-print(wifiSerial.isOpen())
-
 timeStamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print(timeStamp)
-# Scan APs and return information of APs, including RSSI and MAC, etc.
+
+# Set up and open serial 
+wifiSerial = serial.Serial(comNo, baudrateCom, timeout = 3)
+if False == wifiSerial.isOpen():
+	sys.exit('Open serial error!')  #exit program
+else:
+	print('Open serial success!')
+
+# Scan APs and return information of APs, including RSSI and MAC, etc. apInfo: bytes type
 writeFlag = wifiSerial.write('AT+CWLAP\r\n'.encode()) #AT+CWLAP: scan AP
-apInfoOrigin = wifiSerial.read(3000) #return bytes type
-wifiSerial.close()
+if 0 == writeFlag:
+	sys.exit('Write error!')
+else:
+	apInfoOrigin = wifiSerial.read(3000) #return bytes type
+	wifiSerial.close()
+	if 0 != len(apInfoOrigin):
+		print('Scan AP success!')
+	else:
+		sys.exit('Scan AP error!')
+
+# Write original APs' information to text
 with open('aporigin.txt', 'ab') as f:
-	# f.write(timeStamp)
-	# f.write('\n')
 	f.write(apInfoOrigin)
-	# f.write('\n')
 # print(apInfoOrigin)
-apInfo = str_bytes_conv.bytesToStr(apInfoOrigin) #bytes to str
+
+# Convert bytes to str, apInfo: str type
+apInfo = str_bytes_conv.bytesToStr(apInfoOrigin)
 print(apInfo)
 
-# Extract RSSI, MAC & SSID, {27, 100}: ignore APs without SSID
+# Extract RSSI, MAC & SSID, {27, 100}: ignore APs without SSID, re.findall return list type
 apList = re.findall(r'(["][0-9a-zA-Z].{27,100}")', apInfo, re.M)
 # The quantity of AP scanned
-print(apList)
+# print(apList)
 apQuantity = len(apList)
-print(apQuantity)
+# print(apQuantity)
 
 # Post 30 MACs most
 if apQuantity > 30:
@@ -66,13 +77,11 @@ extractAp = '\r\n'.join(apList)
 apFormatted = formatapinfo.formatAp(extractAp, apQuantity)
 print(apFormatted)
 
-# Http request
-
 location = requests.get(amapUrl, headers)
 print(location.text)
 
 with open('apinfo.txt', 'a') as f:
-	# f.write(timeStamp)
+	f.write(timeStamp)
 	f.write('\n')
 	f.write(apFormatted.replace('|', '\n'))
 	f.write('\n')
